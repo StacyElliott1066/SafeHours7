6c6d656905418c69135b514b091635b88f340a1b
unknown <mr.stacy.m.elliott@gmail.com>
2025-04-12 10:30:12 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.safehours.app',
+  appName: 'safehours',
+  webDir: 'dist'
+};
+
+export default config;
